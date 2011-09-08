e04ceaf5aeb1b25fa91a44d824a8cb39837a01ae
Jacob Appelbaum <jacob@appelbaum.net>
2011-09-08 21:57:22 +0200

    add starttls skel code for ftp/smtp/imap/pop3/xmpp

--- teatime.py
+++ teatime.py
@@ -6,11 +6,17 @@
 # TODO:
 #  TLS/SSL session resumption timing even without a valid session
 #
+#  Optionally set system time
+#
 #  Perhaps extend this beyond the weird TLS side channel/info leak with:
 #  Fetch remote IP date/time
 #  Fetch TCP date/time
 #  Fetch ICMP date/time
 #  Fetch remote FTP date/time with the old "put foo/dir foo" trick
+#  Fetch SNTP ( RFC 2030 )
+#  Fetch SMTP banner
+#  STARTTLS for various protocols (most need TLS for auth)
+#  Daytime
 #
 # For TLS we want the first four bytes of the random value sent by the server
 # This is clasically the remote value of gmt_unix_time
@@ -84,6 +90,50 @@ def tls_time_fetcher(remote_host, remote_port):
   connection.close()
   remote_hex_long_time = binascii.b2a_hex(connection.session.serverRandom[:4])
   remote_long_time = struct.unpack('!l', remote_hex_long_time.decode('hex'))[0]
+  return float(remote_long_time)
+
+def start_tls_xmpp_time_fetcher(remote_host, remote_port):
+  # Open a socket to XMPP server and StartTLS like so:
+  # <?xml version='1.0' ?>\r\n
+  # <stream:stream xmlns:stream='http://etherx.jabber.org/streams' xmlns='jabber:client' to='%s' version='1.0'>\r\n
+  # <starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>\r\n
+  # read until /stream:features for urn:ietf:params:xml:ns:xmpp-tls
+  # if we find <proceed, we're good
+  # StartTLS here and then parse connection.session.serverRandom
+  float remote_long_time = 0.0
+  return float(remote_long_time)
+
+def start_tls_pop3_time_fetcher(remote_host, remote_port):
+  # Open a socket to a pop3 service and StartTLS like so:
+  # "STLS\r\n"
+  # read from the socket and if we see "+OK" - we're golden
+  #  StartTLS here and then parse connection.session.serverRandom
+  float remote_long_time = 0.0
+  return float(remote_long_time)
+
+def start_tls_imap_time_fetcher(remote_host, remote_port):
+  # Open a socket to a imap service and StartTLS like so:
+  # ". STARTTLS\r\n"
+  # read from the socket and if we see ". OK" - we're good to go
+  #  StartTLS here and then parse connection.session.serverRandom
+  float remote_long_time = 0.0
+  return float(remote_long_time)
+
+def start_tls_ftp_time_fetcher(remote_host, remote_port):
+  # Open a socket to a ftp service and StartTLS like so:
+  # "AUTH TLS\r\n"
+  # read from the socket and look for "234 AUTH TLS successful"
+  #  StartTLS here and then parse connection.session.serverRandom
+  float remote_long_time = 0.0
+  return float(remote_long_time)
+
+def start_tls_smtp_time_fetcher(remote_host, remote_port):
+  # Open a socket to a ftp service and StartTLS like so:
+  # "EHLO %s\r\n" where %s is "our" hostname
+  # read from the socket and look for "250" and then write "STARTTLS\r\n"
+  # If we see a "220" - we're good to go.
+  #  StartTLS here and then parse connection.session.serverRandom
+  float remote_long_time = 0.0
   return float(remote_long_time)
 
 # This is a basic HTTPS client time fetcher
